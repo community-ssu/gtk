cd22adade1b9038c9a9a4d05b5a867e825e59c4d
Kimmo Hamalainen <kimmo.hamalainen@nokia.com>
2005-09-13 12:46:40 +0000

    returned earlier code
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@854 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- libosso/autogen.sh
+++ libosso/autogen.sh
@@ -7,8 +7,8 @@ autoconf
 autoheader
 automake-1.7 --add-missing --foreign || automake --add-missing --foreign
 
-#if test x$NOCONFIGURE = x; then
-#  ./configure --enable-maintainer-mode "$@"
-#else
+if test x$NOCONFIGURE = x; then
+  ./configure --enable-maintainer-mode "$@"
+else
   echo Skipping configure process.
-#fi
+fi
