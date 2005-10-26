0f187e56e3e297e84be0d310feb6a5353d2bd817
Jakub Pavelek <jakub.pavelek@nokia.com>
2005-10-26 10:20:46 +0000

    Importing fontconfig
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@1280 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

---
+++ fontconfig/device_symbols.h
@@ -0,0 +1,31 @@
+/*
+ * Device symbols
+ *
+ * Defines unicode values for device symbols of hardware buttons
+ *
+ * This file is licensed under the same license as the package fontconfig
+ *
+ * Copyright 2005 Nokia
+ *
+ * Author: Jakub Pavelek
+ *
+ * */
+
+#ifndef __DEVICE_SYMBOLS_H__
+#define __DEVICE_SYMBOLS_H__
+
+#define HWK_BUTTON_CANCEL 		"<span font_desc='DeviceSymbols' fallback='false'>&#61450;</span>"
+#define HWK_BUTTON_MENU 		"<span font_desc='DeviceSymbols' fallback='false'>&#61451;</span>"
+#define HWK_BUTTON_HOME		"<span font_desc='DeviceSymbols' fallback='false'>&#61452;</span>"
+#define HWK_BUTTON_VIEW		"<span font_desc='DeviceSymbols' fallback='false'>&#61453;</span>"
+#define HWK_BUTTON_POWER		"<span font_desc='DeviceSymbols' fallback='false'>&#61454;</span>"
+#define HWK_BUTTON_INCREASE		"<span font_desc='DeviceSymbols' fallback='false'>&#61455;</span>"
+#define HWK_BUTTON_DECREASE		"<span font_desc='DeviceSymbols' fallback='false'>&#61456;</span>"
+#define HWK_BUTTON_UP		"<span font_desc='DeviceSymbols' fallback='false'>&#61457;</span>"
+#define HWK_BUTTON_DOWN		"<span font_desc='DeviceSymbols' fallback='false'>&#61458;</span>"
+#define HWK_BUTTON_LEFT		"<span font_desc='DeviceSymbols' fallback='false'>&#61459;</span>"
+#define HWK_BUTTON_RIGHT		"<span font_desc='DeviceSymbols' fallback='false'>&#61460;</span>"
+#define HWK_BUTTON_SELECT		"<span font_desc='DeviceSymbols' fallback='false'>&#61461;</span>"
+
+
+#endif
