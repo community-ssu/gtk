e31f1d57208998f8d0ef6009ea90cd5abb72d227
Kimmo Hamalainen <kimmo.hamalainen@nokia.com>
2006-02-10 09:33:16 +0000

    updated changelog, fixed bug in restore script
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@2294 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- osso-app-killer/src/osso-app-killer-restore.sh
+++ osso-app-killer/src/osso-app-killer-restore.sh
@@ -7,7 +7,6 @@ if [ "x$USER" = "xroot" ]; then
 fi
 
 sudo /etc/init.d/af-base-apps stop
-/etc/osso-af-init/ke-recv.sh start
 source /etc/osso-af-init/af-defines.sh
 sudo /etc/osso-af-init/gconf-daemon.sh stop
 exit 0
