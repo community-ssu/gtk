11d596001c2b0c389160881777fdcce0371c41d7
Tapani Palli <tapani.palli@nokia.com>
2006-03-01 10:29:21 +0000

    added hung app handler to matchbox.sh
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@2529 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- hildon-initscripts/matchbox.sh
+++ hildon-initscripts/matchbox.sh
@@ -59,7 +59,7 @@ if [ $START = TRUE ]; then
   fi
 
 # _NET_WM_PING ping handler
-export MB_HUNG_APP_HANDLER=''
+export MB_HUNG_APP_HANDLER='/usr/bin/hn-hung-app-handler'
 export MB_AGGRESSIVE_PING=1
 
   $LAUNCHWRAPPER_NICE start "$SVC" $PROG \
