48aac7257c31de505c49b30c1bf4ebd2d9a192b8
Kimmo Hamalainen <kimmo.hamalainen@nokia.com>
2006-03-01 15:14:59 +0000

    fix
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@2532 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- hildon-initscripts/run-standalone.sh
+++ hildon-initscripts/run-standalone.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ -e /etc/osso-af-init/af-defines.sh ]
-then
+if [ -r /etc/osso-af-init/af-defines.sh ]; then
+  unset AF_DEFINES_SOURCED
   source /etc/osso-af-init/af-defines.sh
 else
-  echo "/etc/osso-af-init/af-defines.sh not found!"
+  echo "$0: /etc/osso-af-init/af-defines.sh is not readable!"
   exit 1
 fi
 
