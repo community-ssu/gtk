8244a1ff44b6e0ce08b915a1c31d7b3dde1cb16e
Marius Vollmer <marius.vollmer@nokia.com>
2006-03-08 18:25:38 +0000

    * utils/check-application-for-uninstall.c: Proper parsing of 	command line options.  Look into /proc to find the executable. 	Include application name in dialog.
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@2651 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- osso-application-installer/utils/check-application-for-uninstall.c
+++ osso-application-installer/utils/check-application-for-uninstall.c
@@ -24,37 +24,117 @@
 
 /* This utility will check whether the application specified via a
    .desktop file is currently running.  If it is, it will pop up a
-   dialog that explains that the user should stop that application
-   before uninstalling.
+   dialog that explains this to the user and asks whether to continue.
 
-   The dialog shown to the user offers to kill the running process (if
-   it can be determined) or to cancel the uninstall or to continue.
-
-   This program exits 0 when the uninstallation should proceed and 1
+   This program exits 0 when the uninstallation should proceed and 111
    when it should be cancelled.
 */
 
-/* First version: just show the dialog all the time and offer no way
-   to kill processes.
-*/
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+#include <glib.h>
 #include <gtk/gtk.h>
 #include <hildon-widgets/hildon-note.h>
 
 int
-main (int argc, char *argv)
+find_exec_pid (const char *file)
+{
+  char fullname[512];
+  char linkbuf[512];
+  int n;
+  char *ptr;
+
+  DIR *d = opendir ("/proc");
+  if (d)
+    {
+      struct dirent *e;
+      while ((e = readdir (d)))
+	{
+	  strcpy (fullname, "/proc/");
+	  strcat (fullname, e->d_name);
+	  strcat (fullname, "/exe");
+	  if ((n = readlink (fullname, linkbuf, 512-1)) >= 0)
+	    {
+	      linkbuf[n] = '\0';
+	      if ((ptr = strstr (linkbuf, file))
+		  && strlen (ptr) == strlen (file))
+		{
+		  int pid = atoi (e->d_name);
+		  closedir (d);
+		  return pid;
+		}
+	    }
+	}
+      closedir (d);
+    }
+
+  return -1;
+}
+
+int
+run_dialog (const char *name)
 {
   GtkWidget *dialog;
-  int response;
+  char *text = g_strdup_printf ("%s is currently running.\n"
+				"Continue anyway?", name);
 
-  gtk_init (&argc, &argv);
+  dialog = hildon_note_new_confirmation (NULL, text);
+  g_free (text);
 
-  dialog = hildon_note_new_confirmation 
-    (NULL,
-     "Some application seems to be running.\n"
-     "Continue anyway?");
+  return gtk_dialog_run (GTK_DIALOG (dialog));
+}
 
-  response = gtk_dialog_run (GTK_DIALOG (dialog));
+char *arg_name = NULL;
+char *arg_exe = NULL;
 
-  exit ((response == GTK_RESPONSE_OK)? 0 : 1);
+static GOptionEntry entries[] = {
+  { "application-name", 'a', 0, G_OPTION_ARG_STRING, &arg_name,
+    "Application name", "app" },
+  { "exe-file", 'x', 0, G_OPTION_ARG_STRING, &arg_exe,
+    "Executable filename", "file" },
+  { NULL }
+};
+
+int
+main (int argc, char **argv)
+{
+  GError *error = NULL;
+  GOptionContext *context;
+
+  int pid, code = 0;
+
+  context = g_option_context_new ("- check for running applications");
+  g_option_context_add_main_entries (context, entries, NULL);
+  g_option_context_add_group (context, gtk_get_option_group (TRUE));
+  g_option_context_parse (context, &argc, &argv, &error);
+
+  if (error)
+    {
+      fprintf (stderr, "check-application-for-uninstall: %s\n",
+	       error->message);
+      exit (1);
+    }
+
+  if (arg_name == NULL)
+    {
+      fprintf (stderr, "check-application-for-uninstall: Missing -a option\n");
+      exit (1);
+    }
+
+  if (arg_exe == NULL)
+    {
+      fprintf (stderr, "check-application-for-uninstall: Missing -x option\n");
+      exit (1);
+    }
+
+  pid = find_exec_pid (arg_exe);
+
+  if (pid >= 0)
+    code = (run_dialog (arg_name) == GTK_RESPONSE_OK)? 0 : 111;
+
+  exit (code);
 }
