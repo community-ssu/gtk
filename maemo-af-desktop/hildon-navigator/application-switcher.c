5de782a7d99bfbf0cfeeb16263d8af32bbc63878
Johan Bilien <johan.bilien@nokia.com>
2006-03-13 15:21:16 +0000

    2006-03-13  Johan Bilien <johan.bilien@nokia.com>
    	* hildon-navigator/application-switcher.c: Use GTK_WINDOW_POPUP
    	  instead of GTK_WINDOW_TOPLEVEL for the tooltip (Closes N#19590)
    
    
    
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@2711 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- maemo-af-desktop/hildon-navigator/application-switcher.c
+++ maemo-af-desktop/hildon-navigator/application-switcher.c
@@ -295,7 +295,7 @@ void application_switcher_initialize_menu(ApplicationSwitcher_t *as)
     gtk_widget_set_name(GTK_WIDGET(as->menu), HILDON_NAVIGATOR_MENU_NAME);
     
     /* Create a tooltip menu. Small icons buttons uses this menu. */
-    as->tooltip_menu = gtk_window_new(GTK_WINDOW_TOPLEVEL); 
+    as->tooltip_menu = gtk_window_new(GTK_WINDOW_POPUP); 
     gtk_widget_set_name(GTK_WIDGET(as->tooltip_menu), 
                         "hildon-task-navigator-tooltip");
     gtk_window_set_decorated(GTK_WINDOW(as->tooltip_menu), FALSE);
