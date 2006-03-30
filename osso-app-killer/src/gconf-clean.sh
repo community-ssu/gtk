116c9cd94f71b301f1fc3ba49e8ea8d88f834420
Kimmo Hamalainen <kimmo.hamalainen@nokia.com>
2006-03-30 09:49:02 +0000

    adding 'proxy' string to CUD grep
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@3132 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- osso-app-killer/src/gconf-clean.sh
+++ osso-app-killer/src/gconf-clean.sh
@@ -8,7 +8,8 @@ if [ $? = 0 ]; then
     if [ "x$CUD" != "x" ]; then
       if [ "x$d" = "xsystem" ]; then
         for f in `find system -name *.xml`; do
-          echo "$f" | grep -e '\(connectivity\)\|\(bluetooth\)' > /dev/null
+          echo "$f" | grep -e '\(connectivity\)\|\(bluetooth\)\|\(proxy\)' \
+	              > /dev/null
           if [ $? = 1 ]; then
             echo "$0: removing $f"
             rm -f $f
