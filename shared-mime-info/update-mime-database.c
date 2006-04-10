02f9756b460d3ce15701533e8ea94c37598e311f
Erik Karlsson <erik.b.karlsson@nokia.com>
2006-04-10 10:05:38 +0000

    Added GPL header to update-mime-database.c
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@3519 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- shared-mime-info/update-mime-database.c
+++ shared-mime-info/update-mime-database.c
@@ -1,3 +1,28 @@
+/* update-mime-database.c 
+ *
+ * Copyright 2004, 2005  
+ * Jonathan Blandford  <jrb@redhat.com>,
+ * Matthias Clasen <mclasen@redhat.com>, 
+ * Christian Neumair  <chris@gnome-de.org>
+ * Christophe Fergeau  <teuf@gnome.org>
+ * 
+ * update-mime-database is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * update-desktop-database is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with update-desktop-database; see the file COPYING.  If not,
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite
+ * 330, Boston, MA 02111-1307, USA.
+ */
+
+
 #include <config.h>
 
 #define N_(x) x
