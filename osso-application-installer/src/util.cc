d17a8903d81b11d991c5520b1260a2413ee0b2f1
Marius Vollmer <marius.vollmer@nokia.com>
2006-04-14 19:44:15 +0000

    * src/util.cc (make_global_package_list): Increase refcount of 	global_section_list.  Otherwise it becomes invalid when the widget 	is destroyed. 	(clear_global_section_list): Unref it.
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@3675 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- osso-application-installer/src/util.cc
+++ osso-application-installer/src/util.cc
@@ -914,6 +914,7 @@ make_global_section_list (GList *sections, section_activated *act)
 					 hbox);
 
   global_section_list = scroller;
+  g_object_ref (scroller);
 
   return scroller;
 }
@@ -923,6 +924,7 @@ clear_global_section_list ()
 {
   if (global_section_list)
     gtk_widget_destroy (global_section_list);
+  g_object_unref (global_section_list);
   global_section_list = NULL;
 }
 
