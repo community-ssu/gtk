10ecb6877a4787a6853e2eb5113de82426b8ffd2
Tommi Komulainen <tommi.komulainen@nokia.com>
2006-06-02 11:56:10 +0000

    #343677: Chain up to key_release_event, not key_press_event
    2006-06-02  Tommi Komulainen  <tommi.komulainen@nokia.com>
    	
    	* gtk/gtkwindow.c (gtk_window_key_release_event): Chain up to
    	key_release_event, not key_press_event (#343677)
    
    
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/maemo-branches/IT-2006/gtk+@4727 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -4554,7 +4554,7 @@ gtk_window_key_release_event (GtkWidget   *widget,
 
   /* Chain up, invokes binding set */
   if (!handled)
-    handled = GTK_WIDGET_CLASS (parent_class)->key_press_event (widget, event);
+    handled = GTK_WIDGET_CLASS (parent_class)->key_release_event (widget, event);
 
   return handled;
 }
