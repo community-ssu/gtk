fe2615470332a1a087ce5b1dd29c93a289267892
Richard Sun <richard.sun@nokia.com>
2006-09-21 07:26:05 +0000

    NB#40651.
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@6567 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- osso-gnome-vfs2/dbus-daemon/vfs-daemon.c
+++ osso-gnome-vfs2/dbus-daemon/vfs-daemon.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
- * Copyright (C) 2004-2005 Nokia Corporation.
+ * Copyright (C) 2004-2006 Nokia Corporation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License as
