ef50be0fe110e1e9b42f43dc48eb08614e1f95b2
Moises Martinez <moises.martinez@nokia.com>
2006-09-21 16:42:57 +0000

    * install_sbapplet.sh: Added script to help sb's           plugins packages install its plugin.         * debian/maemo_af_desktop.install: Added entry           for install_sbapplet.sh 	* Makefile.am updated 	* Changelog updated
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@6596 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

---
+++ maemo-af-desktop/install_sbapplet.sh
@@ -0,0 +1,62 @@
+#!/bin/sh -e
+# moimart
+
+if [ ! "$#" -eq "4" ];
+then
+  echo "4 args required"
+  echo "desktopfile library category mandatory"
+  exit 1;
+fi
+
+DESKTOP_FILE=$1
+LIBRARY=$2
+CATEGORY=$3
+
+case $4 in
+  [0-1])
+    MANDATORY=$4
+    ;;
+  [tT]rue)
+    MANDATORY=true
+    ;;
+  [fF]alse)
+    MANDATORY=false
+   ;;
+  *)
+    MANDATORY=false
+    ;;
+esac
+
+SB_CONF=plugins.conf
+SB_USRDIRCONF=/home/user/.osso/hildon-status-bar/$SB_CONF
+SB_SYSDIRCONF=/etc/hildon-status-bar/$SB_CONF
+SB_PATH=$SB_USRDIRCONF
+
+if [ ! -r $SB_PATH ];
+then
+  cp $SB_SYSDIRCONF $SB_USRDIRCONF
+  echo "cp $SB_SYSDIRCONF $SB_USRDIRCONF"
+fi
+
+if [ ! -r $SB_PATH ];
+then
+  echo "Can't access configuration"
+  exit 0
+fi
+
+if [ "`cat $SB_PATH|grep \"\[$DESKTOP_FILE\]\"|wc -l`" -gt "0" ];
+then
+  echo "Entry already configured"
+  exit 0;
+fi
+
+echo "Creating entry..."
+
+POSITION=`cat $SB_PATH|grep Position|wc -l`
+
+echo -e "[$DESKTOP_FILE]\n\
+Library=$LIBRARY\n\
+Desktop-file=$DESKTOP_FILE\n\
+Category=$CATEGORY\n\
+Position=$POSITION\n\
+Mandatory=$MANDATORY\n" >> $SB_PATH
