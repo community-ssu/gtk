89a7cc9f0123b2cd73a868f4598cecaecec85c43
Guillem Jover <guillem.jover@nokia.com>
2006-09-28 00:35:39 +0000

    Add new maemo-summoner, to load directly .launch applications. (Closes: #38580@Nokia)
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@6793 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- maemo-launcher/launcher/launcher.c
+++ maemo-launcher/launcher/launcher.c
@@ -36,35 +36,19 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/resource.h>
-#ifdef HAVE_PRCTL_SET_NAME
-#include <sys/prctl.h>
-#endif
 #include <fcntl.h>
 #include <signal.h>
 #include <unistd.h>
-#include <dlfcn.h>
 #include <errno.h>
 
 #include "report.h"
 #include "invokelib.h"
 #include "comm_dbus.h"
 #include "booster.h"
+#include "prog.h"
 
 /* FIXME: Should go into '/var/run/'. */
 #define LAUNCHER_PIDFILE "/tmp/"PROG_NAME".pid"
-
-typedef int (*entry_t)(int, char **);
-
-typedef struct
-{
-  int options;
-  int argc;
-  char **argv;
-  char *filename;
-  char *name;
-  int prio;
-  entry_t entry;
-} prog_t;
 
 typedef struct
 {
@@ -85,27 +69,6 @@ static pid_t is_parent = 1;
 static volatile bool sigchild_catched = false;
 static volatile bool sighup_catched = false;
 static bool send_app_died = false;
-
-#ifdef DEBUG
-extern char **environ;
-
-static void
-print_prog_env_argv(prog_t *prog)
-{
-  int i;
-
-  for (i = 0; environ[i] != NULL; i++)
-    debug("env[%i]='%s'\n", i, environ[i]);
-
-  for (i = 0; i < prog->argc; i++)
-    debug("argv[%i]='%s'\n", i, prog->argv[i]);
-}
-#else
-static inline void
-print_prog_env_argv(prog_t *prog)
-{
-}
-#endif
 
 static bool
 rise_oom_defense(pid_t pid)
@@ -132,24 +95,6 @@ rise_oom_defense(pid_t pid)
     error("rising the oom shield for pid=%d status=%d\n", pid, status);
     return false;
   }
-}
-
-static void
-load_main(prog_t *prog)
-{
-  void *module;
-  char *error_s;
-
-  /* Load the launched application. */
-  module = dlopen(prog->filename, RTLD_LAZY | RTLD_GLOBAL);
-  if (!module)
-    die(1, "loading invoked application: '%s'\n", dlerror());
-
-  dlerror();
-  prog->entry = (entry_t)dlsym(module, "main");
-  error_s = dlerror();
-  if (error_s != NULL)
-    die(1, "loading symbol 'main': '%s'\n", error_s);
 }
 
 static void
@@ -180,36 +125,6 @@ launch_process(prog_t *prog)
 #endif
 
   _exit(prog->entry(prog->argc, prog->argv));
-}
-
-#ifdef HAVE_PRCTL_SET_NAME
-static void
-set_process_name(const char *progname)
-{
-  prctl(PR_SET_NAME, basename(progname));
-}
-#else
-static inline void
-set_process_name(const char *progname)
-{
-}
-#endif
-
-static void
-set_progname(char *progname, int argc, char **argv)
-{
-  int argvlen = 0;
-  int i;
-
-  for (i = 0; i < argc; i++)
-    argvlen += strlen(argv[i]) + 1;
-
-  memset(argv[0], 0, argvlen);
-  strncpy(argv[0], progname, argvlen - 1);
-
-  set_process_name(progname);
-
-  setenv("_", progname, true);
 }
 
 static int
---
+++ maemo-launcher/launcher/summoner.c
@@ -0,0 +1,125 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2006 Nokia
+ *
+ * Authors: Guillem Jover <guillem.jover@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <sys/resource.h>
+//#include <unistd.h>
+
+#include "config.h"
+#include "report.h"
+#include "prog.h"
+
+static void
+summon_process(prog_t *prog)
+{
+  load_main(prog);
+
+#ifdef DEBUG
+  print_env_args(prog);
+
+  debug("summoning process: '%s'\n", prog->filename);
+#endif
+
+  _exit(prog->entry(prog->argc, prog->argv));
+}
+
+static void
+version(void)
+{
+  printf("%s (%s) %s\n", PROG_NAME, PACKAGE, VERSION);
+
+  exit(0);
+}
+
+static void
+usage(int status)
+{
+  printf("Usage: %s [options] file [file-options]\n"
+	 "\n"
+	 "Options:\n"
+	 "  --version           Print program version.\n"
+	 "  --help              Print this help message.\n",
+	 PROG_NAME);
+
+  exit(status);
+}
+
+int
+main(int argc, char *argv[])
+{
+  int i;
+  prog_t prog = { 0 };
+
+  if (strstr(argv[0], PROG_NAME))
+  {
+    /* Called with our default name. Parse arguments. */
+    for (i = 1; i < argc; ++i)
+    {
+      if (strcmp(argv[i], "--version") == 0)
+	version();
+      else if (strcmp(argv[i], "--help") == 0)
+	usage(0);
+      else if (strncmp(argv[i], "--", 2) == 0)
+	usage(1);
+      else
+      {
+	prog.filename = strdup(argv[i]);
+	prog.argc = argc - i;
+	prog.argv = &argv[i];
+	break;
+      }
+    }
+
+    if (!prog.filename)
+      usage(1);
+
+    set_progname(prog.argv[0], argc, argv);
+  }
+  else
+  {
+    char *launch = NULL;
+
+    /* Called with a different name. Add the proper extension and launch it.
+     * Do not try to parse any arguments. */
+    asprintf(&launch, "%s.launch", argv[0]);
+    prog.filename = launch;
+    prog.argc = argc;
+    prog.argv = argv;
+  }
+
+  /* Summon it. */
+  if (prog.filename)
+  {
+    info("summoning '%s'\n", prog.filename);
+    summon_process(&prog);
+  }
+  else
+    error("nothing to summon\n");
+
+  _exit (0);
+}
+
