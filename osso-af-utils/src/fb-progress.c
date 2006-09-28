d2ac02fd09e234f1505bfbba6bacd3d88ea79595
Kimmo Hamalainen <kimmo.hamalainen@nokia.com>
2006-09-28 15:22:03 +0000

    fixed copyright headers
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@6835 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- osso-af-utils/src/fb-progress.c
+++ osso-af-utils/src/fb-progress.c
@@ -55,14 +55,13 @@
  * under the terms specified in the GNU Public Licence (GPL).
  *
  * Copyright (C) 1998 Torsten Scherer (fb init and its signal handling)
- * Copyright (C) 2005,2006 Nokia Corporation
+ * Copyright (C) 2005-2006 Nokia Corporation. All rights reserved.
+ *
  * Author: Eero Tamminen
- * Contact: Kimmo Hämäläinen <kimmo.hamalainen@nokia.com>
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -71,8 +70,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- * USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  */
 
 #include <stdio.h>
--- osso-af-utils/src/temp-reaper.c
+++ osso-af-utils/src/temp-reaper.c
@@ -1,24 +1,24 @@
 /*
  * Daemon to clean up orphaned temporary files.
  *
- * Copyright (C) 2005-2006 Nokia Corporation
  *
- * Guillem Jover <guillem.jover@nokia.com>
+ * Copyright (C) 2005-2006 Nokia Corporation. All rights reserved.
+ * 
+ * Contact: Guillem Jover <guillem.jover@nokia.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *  
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  */
 
 #include <stdio.h>
