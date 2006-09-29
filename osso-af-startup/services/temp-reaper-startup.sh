a3c2c5bca518fc32e6c9ecdb483ca57daadc91c4
Kimmo Hamalainen <kimmo.hamalainen@nokia.com>
2006-09-29 07:38:20 +0000

    fixed some copyright headers
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@6842 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- osso-af-startup/services/temp-reaper-startup.sh
+++ osso-af-startup/services/temp-reaper-startup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# Temp. file purging script startup/shutdown script
-# Copyright (C) 2004-2005 Nokia Corporation.
+# Temporary file purging daemon startup/shutdown script
+#
+# Copyright (C) 2004-2006 Nokia Corporation. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,9 +14,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-# USA
-
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
 
 if [ "x$LAUNCHWRAPPER_NICE_TRYRESTART" = "x" ]; then
   echo "$0: Error, LAUNCHWRAPPER_NICE_TRYRESTART is not defined"
