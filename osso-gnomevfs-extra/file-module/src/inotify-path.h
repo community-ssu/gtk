f101e255ba9381f40a3affd2ad482be12a93e3e8
Richard Sun <richard.sun@nokia.com>
2006-10-19 06:53:05 +0000

    Update files from upstream.
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@7338 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- osso-gnomevfs-extra/file-module/src/inotify-path.h
+++ osso-gnomevfs-extra/file-module/src/inotify-path.h
@@ -1,17 +1,23 @@
 /*
-	Copyright (C) 2006 John McCutchan <john@johnmccutchan.com>
+   Copyright (C) 2005 John McCutchan
 
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; version 2.
+   The Gnome Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License version 2 for more details.
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software Foundation,
-	Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+   The Gnome Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the Gnome Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+   Authors:.
+		John McCutchan <john@johnmccutchan.com>
 */
 
 #ifndef __INOTIFY_PATH_H
