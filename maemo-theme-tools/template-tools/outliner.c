eb451d0d93fb4b85f91d5fba99ad634474db4612
Michael Dominic Kostrzewa <michael.kostrzewa@nokia.com>
2006-11-01 13:14:28 +0000

    2006-11-01  Michael Dominic Kostrzewa  <michael.kostrzewa@nokia.com>
    	* template-tools/Makefile.am:
    	* template-tools/outliner.c:
    	* template-tools/outliner.h: Adding the outliner tool that can be used
    	to generate the outlines to be used as guides when drawing the theme.
    
    	* template-tools/regenerator.c: Correcting a bug resulting in extra
    	critical warnings about unreffing (when image missing). 
    
    
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@7768 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- maemo-theme-tools/template-tools/outliner.c
+++ maemo-theme-tools/template-tools/outliner.c
@@ -20,12 +20,13 @@
  *
  */
 
-#include "regenerator.h"
+#include "outliner.h"
 
 /* Do the work */
-GdkPixbuf*                      process (Template *templ, gchar *directory)
+GdkPixbuf*                      process (Template *templ)
 {
         GSList *iterator = NULL;
+        int counter = 0;
         GdkPixbuf *composite_image = gdk_pixbuf_new (GDK_COLORSPACE_RGB, TRUE, 8, templ->Width, templ->Height);
         
         g_return_val_if_fail (templ != NULL, NULL);
@@ -36,14 +37,8 @@ GdkPixbuf*                      process (Template *templ, gchar *directory)
         /* Reset the pixbuf to transparent white */
         gdk_pixbuf_fill (composite_image, 0xffffff00);
 
-        if (directory == NULL)
-                directory = "./";
-
         for (iterator = templ->ElementList; iterator; iterator = g_slist_next (iterator)) {
                 Element *element = (Element *) iterator->data;
-                gchar *fname = (element->Name != NULL) ? 
-                                g_build_filename (directory, element->Name, NULL) : 
-                                NULL;
                 GdkPixbuf *image = NULL;
 
                 if (element->X > templ->Width || 
@@ -52,28 +47,31 @@ GdkPixbuf*                      process (Template *templ, gchar *directory)
                     element->Y + element->Height > templ->Height) {
                         g_warning ("Element '%s' is out of bounds (%d %d %d %d)!", element->Name,  
                                    element->X, element->Y, element->Width, element->Height);
-                } else if (! g_file_test (fname, G_FILE_TEST_EXISTS)) {
-                        g_warning ("Element file name '%s' not found, using PINK!", element->Name);
-                        image = gdk_pixbuf_new (GDK_COLORSPACE_RGB, TRUE, 8, element->Width, element->Height);
-                        gdk_pixbuf_fill (image, 0xff00ffff);
                 } else { 
-                        image = gdk_pixbuf_new_from_file (fname, NULL);
-                        if (image == NULL) 
-                                g_warning ("Failed to load element image '%s', ignoring!", 
-                                           element->Name);
+                        image = gdk_pixbuf_new (GDK_COLORSPACE_RGB, TRUE, 8, element->Width, element->Height);
+
+                        if (counter % 5 == 0) 
+                                gdk_pixbuf_fill (image, 0x00ff00aa);
+                        else if (counter % 5 == 1)
+                                gdk_pixbuf_fill (image, 0x00ee00aa);
+                        else if (counter % 5 == 2)
+                                gdk_pixbuf_fill (image, 0x00dd00aa);
+                        else if (counter % 5 == 3)
+                                gdk_pixbuf_fill (image, 0x00cc00aa);
+                        else
+                                gdk_pixbuf_fill (image, 0x00bb00aa);
                 }
 
                 if (image != NULL) {
                         /* Draw the stuff over the composite image */
                         gdk_pixbuf_copy_area (image, 0, 0, element->Width, element->Height, 
                                               composite_image, element->X, element->Y);
-                        g_print ("PROCESSED %s\n", element->Name);
+                        g_print ("OUTLINED %s\n", element->Name);
+                        
                         gdk_pixbuf_unref (image);
                 }
 
-                if (fname != NULL)
-                        g_free (fname);
-
+                counter++;
         }
 
         return composite_image;
@@ -82,24 +80,23 @@ GdkPixbuf*                      process (Template *templ, gchar *directory)
 /* Shows the initial copyright/info banner */
 void                            show_banner (void)
 {
-        g_print ("Regenerator tool by Michael Dominic K. and Tuomas Kuosmanen\n");
+        g_print ("Outliner tool by Michael Dominic K. and Tuomas Kuosmanen\n");
         g_print ("Copyright 2006 by Nokia Corporation.\n\n");
 }
 
 /* Show some info about basic usage of the tool */
 void                            show_usage (void)
 {
-        g_print ("Usage: regenerate <template> <outputimage> [inputdir]\n\n");
-        g_print ("This tool will combine individual images (ie. previously sliced) into one huge\n"
-                 "image template as specified by the layout template. Optionally you can specify an\n"
-                 "input directory. The tool can load in PNG and JPEG formats.\n\n");
+        g_print ("Usage: outline <template> <outputimage>\n\n");
+        g_print ("This tool will create an outline image that shows the slicing guides. \n"
+                 "You can use this image in your graphics program to check if your drawings\n"
+                 "fit the proper areas. \n\n");
 }
 
 /* This is the place where we came in... */
 int                             main (int argc, char **argv)
 {
         gchar *template_file = NULL;
-        gchar *directory = NULL;
         gchar *output_image_file = NULL;
         int return_val = 0;
         Template *template = NULL;
@@ -128,10 +125,6 @@ int                             main (int argc, char **argv)
         if (! g_file_test (template_file, G_FILE_TEST_EXISTS)) 
                 g_error ("%s not found!", template_file);
 
-        /* Check the optional directory argument */
-        if (argc >= 4 && argv [3] != NULL) 
-                directory = argv [3];
-
         /* Read the template file. That spits out errors too */
         template = read_template (template_file);
 
@@ -141,7 +134,7 @@ int                             main (int argc, char **argv)
         /* Basic info */
         show_template (template);
 
-        output_image = process (template, directory);
+        output_image = process (template);
         g_printf ("\n");
 
         /* Save the resulting image */
