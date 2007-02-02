36f7f3b557fa68c8285b61a5bd171c6a3ea31149
Marius Vollmer <marius.vollmer@nokia.com>
2007-02-02 15:45:49 +0000

    * autogen.sh: Don't call configure.  Always use generic versions 	of the tools instead of specific ones.
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@9589 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- hildon-fm/autogen.sh
+++ hildon-fm/autogen.sh
@@ -2,13 +2,7 @@
 
 set -x
 libtoolize --automake
-aclocal-1.7 || aclocal
+aclocal
 autoconf
 autoheader
-automake-1.7 --add-missing --foreign || automake --add-missing --foreign
-
-if test x$NOCONFIGURE = x; then
-  ./configure --enable-maintainer-mode "$@"
-else
-  echo Skipping configure process.
-fi
+automake --add-missing --foreign
