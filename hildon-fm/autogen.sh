4eae49eb31eac81fc73d48b140249778ddffc19e
Marius Vollmer <marius.vollmer@nokia.com>
2007-02-08 13:36:49 +0000

    * autogen.sh: Put back invocation of configure since that is 	apparently expected to happen by some automated build systems. 	Thanks to Richard Hult for pointing this out!
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@9749 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- hildon-fm/autogen.sh
+++ hildon-fm/autogen.sh
@@ -6,3 +6,9 @@ aclocal
 autoconf
 autoheader
 automake --add-missing --foreign
+
+if test x$NOCONFIGURE = x; then
+  ./configure --enable-maintainer-mode "$@"
+else
+  echo Skipping configure process.
+fi
