06d4b8d60ee0597b58c2d71c475725de0e484d20
Fernando Herrera <fernando.herrera-de-las-heras@nokia.com>
2007-04-23 14:14:32 +0000

    * hail/hail-hildon-desktop/Makefile.am:         * hail/hail-hildon-desktop/hail-hildon-desktop.c:         Added the management for the new a11y added.
            * hail/hail-hildon-desktop/hailtogglebutton.[ch]:
            Added HNAppButton a11y implementation.
    
            * hail/hail-hildon-desktop/hailhometitlebar.[ch]:
            Added HildonHomeTitlebar a11y implementation.
    
            * hail/hail-hildon-desktop/hailhomearea.[ch]:
            Added HildonHomeArea a11y implementation.
    
            * hail/hail-hildon-desktop/haildesktopitem.c:
            Fixed the value for the default description string.
    
    
    
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@11200 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- hail/hail/hail-hildon-desktop/hail-hildon-desktop.c
+++ hail/hail/hail-hildon-desktop/hail-hildon-desktop.c
@@ -8,6 +8,9 @@
 #include "haildesktopwindow.h"
 #include "hailhomewindow.h"
 #include "haildesktoppanelwindow.h"
+#include "hailtogglebutton.h"
+#include "hailhomearea.h"
+#include "hailhometitlebar.h"
 
 #include <gtk/gtkimagemenuitem.h>
 #include <libhildondesktop/hildon-desktop-item.h>
@@ -16,6 +19,9 @@
 #include <libhildondesktop/hildon-desktop-window.h>
 #include <libhildondesktop/hildon-home-window.h>
 #include <libhildondesktop/hildon-desktop-panel-window.h>
+#include <gtk/gtktogglebutton.h>
+#include <libhildondesktop/hildon-home-area.h>
+#include <libhildondesktop/hildon-home-titlebar.h>
 
 /* Hail factories class definition */
 HAIL_ACCESSIBLE_FACTORY (HAIL_TYPE_IMAGE_MENU_ITEM, hail_image_menu_item, hail_image_menu_item_new)
@@ -25,7 +31,9 @@ HAIL_ACCESSIBLE_FACTORY (HAIL_TYPE_DESKTOP_PANEL_ITEM, hail_desktop_panel_item, 
 HAIL_ACCESSIBLE_FACTORY (HAIL_TYPE_DESKTOP_WINDOW, hail_desktop_window, hail_desktop_window_new)
 HAIL_ACCESSIBLE_FACTORY (HAIL_TYPE_HOME_WINDOW, hail_home_window, hail_home_window_new)
 HAIL_ACCESSIBLE_FACTORY (HAIL_TYPE_DESKTOP_PANEL_WINDOW, hail_desktop_panel_window, hail_desktop_panel_window_new)
-
+HAIL_ACCESSIBLE_FACTORY (HAIL_TYPE_TOGGLE_BUTTON, hail_toggle_button, hail_toggle_button_new)
+HAIL_ACCESSIBLE_FACTORY (HAIL_TYPE_HOME_AREA, hail_home_area, hail_home_area_new)
+HAIL_ACCESSIBLE_FACTORY (HAIL_TYPE_HOME_TITLEBAR, hail_home_titlebar, hail_home_titlebar_new)
 
 void
 hail_hildon_desktop_init (void)
@@ -37,4 +45,7 @@ hail_hildon_desktop_init (void)
   HAIL_WIDGET_SET_FACTORY (HILDON_DESKTOP_TYPE_WINDOW, hail_desktop_window);
   HAIL_WIDGET_SET_FACTORY (HILDON_TYPE_HOME_WINDOW, hail_home_window);
   HAIL_WIDGET_SET_FACTORY (HILDON_DESKTOP_TYPE_PANEL_WINDOW, hail_desktop_panel_window);
+  HAIL_WIDGET_SET_FACTORY (GTK_TYPE_TOGGLE_BUTTON, hail_toggle_button);
+  HAIL_WIDGET_SET_FACTORY (HILDON_TYPE_HOME_AREA, hail_home_area);
+  HAIL_WIDGET_SET_FACTORY (HILDON_TYPE_HOME_TITLEBAR, hail_home_titlebar);
 }
