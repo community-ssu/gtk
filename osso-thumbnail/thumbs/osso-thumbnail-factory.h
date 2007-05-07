a50d6f45bb8a78279ef3e6bdfdb8a99c1cae963e
Marius Vollmer <marius.vollmer@nokia.com>
2007-05-07 11:45:28 +0000

    * thumbs/osso-thumber-common.h, 	thumbs/osso-thumber-common-private.h: Renamed former to latter and 	use latter in our own code.  Make former unusable for public use. 	* thumbs/osso-thumbnail-factory.h 	thumbs/osso-thumbnail-factory-private.h: Likewise.
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@11486 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

---
+++ osso-thumbnail/thumbs/osso-thumbnail-factory.h
@@ -0,0 +1,24 @@
+/*
+ * This file is part of osso-thumbnail package
+ *
+ * Copyright (C) 2007 Nokia Corporation.  All Rights reserved.
+ *
+ * Contact: Marius Vollmer <marius.vollmer@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#error The osso-thumbnail library is no longer supported.  Use libhildon-thumbnail instead.
