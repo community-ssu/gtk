ec2a6e6eb79a840fc1c01d0e18e1fd4de0a45940
Kimmo Hamalainen <kimmo.hamalainen@nokia.com>
2007-05-15 09:53:45 +0000

    moved to org.freedesktop.Notifications API
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@11645 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- libosso/src/osso-system-note.c
+++ libosso/src/osso-system-note.c
@@ -1,11 +1,11 @@
 /**
  * @file osso-system-note.c
  * This file implements D-Bus requests for system notification
- * dialogs and infoprints (on statusbar)
+ * dialogs and infoprints
  * 
- * This file is part of libosso
+ * This file is part of Libosso
  *
- * Copyright (C) 2005 Nokia Corporation. All rights reserved.
+ * Copyright (C) 2005-2007 Nokia Corporation. All rights reserved.
  *
  * Contact: Kimmo Hämäläinen <kimmo.hamalainen@nokia.com>
  *
@@ -27,6 +27,10 @@
 #include <osso-log.h>
 #include "osso-internal.h"
 
+#define FDO_SERVICE "org.freedesktop.Notifications"
+#define FDO_OBJECT_PATH "/org/freedesktop/Notifications"
+#define FDO_INTERFACE "org.freedesktop.Notifications"
+
 osso_return_t osso_system_note_dialog(osso_context_t *osso,
 				      const gchar *message,
 				      osso_system_note_type_t type,
@@ -34,7 +38,7 @@ osso_return_t osso_system_note_dialog(osso_context_t *osso,
 {
   gint rpc_retval = OSSO_OK;
 
-  if ( (osso == NULL) || (message == NULL) ) {
+  if (osso == NULL || message == NULL) {
     ULOG_ERR_F("Invalid osso context or message");
     return OSSO_INVALID;
   }
@@ -43,13 +47,15 @@ osso_return_t osso_system_note_dialog(osso_context_t *osso,
     ULOG_ERR_F("Invalid message type");
     return OSSO_INVALID;
   }
-  rpc_retval = osso_rpc_run_with_defaults(osso, OSSO_BUS_STATUSBAR,
-					  OSSO_BUS_SYSNOTE,
-					  retval, DBUS_TYPE_STRING,
-					  message, DBUS_TYPE_INT32, type,
-					  DBUS_TYPE_INVALID);
+  rpc_retval = osso_rpc_run(osso, FDO_SERVICE, FDO_OBJECT_PATH,
+                            FDO_INTERFACE, "SystemNoteDialog",
+                            retval, DBUS_TYPE_STRING, message,
+                            DBUS_TYPE_INT32, type,
+                            DBUS_TYPE_STRING, "",
+                            DBUS_TYPE_INVALID);
+
   if (rpc_retval != OSSO_OK) {
-    ULOG_ERR_F("RPC call for system note dialog failed");
+    ULOG_ERR_F("osso_rpc_run failed");
     return OSSO_ERROR;
   }
   return OSSO_OK;
@@ -61,18 +67,18 @@ osso_return_t osso_system_note_infoprint(osso_context_t *osso,
 {
   gint rpc_retval = 0;
   
-  if ( (osso == NULL) || (text == NULL) ) {
+  if (osso == NULL || text == NULL) {
     ULOG_ERR_F("Invalid osso context or message text");
     return OSSO_INVALID;
   }
     
-  rpc_retval = osso_rpc_run_with_defaults(osso, OSSO_BUS_STATUSBAR,
-					  OSSO_BUS_INFOPRINT,
-					  retval, DBUS_TYPE_STRING, text,
-					  DBUS_TYPE_INVALID);
+  rpc_retval = osso_rpc_run(osso, FDO_SERVICE, FDO_OBJECT_PATH,
+                            FDO_INTERFACE, "SystemNoteInfoprint",
+                            retval, DBUS_TYPE_STRING, text,
+                            DBUS_TYPE_INVALID);
 
   if (rpc_retval != OSSO_OK) {
-    ULOG_ERR_F("RPC call for statusbar infoprint failed");
+    ULOG_ERR_F("osso_rpc_run failed");
     return OSSO_ERROR;
   }
   return OSSO_OK;
