c3d9f7e768a722d80e6fe4285ed2d23cce3b719e
Moises Martinez <moises.martinez@nokia.com>
2007-06-12 17:59:51 +0000

    2007-06-12  Moises Martinez  <moises.martinez@nokia.com
            * src/main.c: Fixed problem with thread initialization with glib.
            Pointed out by Tommi.
    	* ChangeLog updated.
    
    
    
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@12258 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- hildon-desktop/src/main.c
+++ hildon-desktop/src/main.c
@@ -42,6 +42,10 @@ int main (int argc, char **argv)
   GObject *desktop;
   gchar *gtkrc = NULL;
 
+  g_thread_init (NULL);
+  gdk_threads_init ();
+  gdk_threads_enter ();
+
   setlocale (LC_ALL, "");
 
   bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
@@ -81,6 +85,7 @@ int main (int argc, char **argv)
   hd_desktop_run (HD_DESKTOP (desktop));
 
   gtk_main ();
+  gdk_threads_leave ();
 
   return 0;
 }
