290fc4f945c37c86af5fcb4b9941c09a9e3d49b1
Xan Lopez <xan.lopez@nokia.com>
2007-06-14 09:06:13 +0000

    2007-06-13  Tomas Junnonen  <tomas.junnonen@nokia.com>
    	* gtk/gtkenums.h, gtk/gtktextview.c: Add HILDON_GTK_INPUT_MODE_MULTILINE
    	to hint widgets capable of receiving multiple lines of text.
    
    2007-05-30  Tomas Junnonen  <tomas.junnonen@nokia.com>
    
    	* gtk/tmpl/gtkimcontext.sgml: Add HILDON_GTK_INPUT_MODE_MULTILINE
    
    
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@12288 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- gtk+/gtk/gtktextview.c
+++ gtk+/gtk/gtktextview.c
@@ -663,6 +663,7 @@ gtk_text_view_class_init (GtkTextViewClass *klass)
                                                        P_("Define widget's input mode"),
                                                        GTK_TYPE_GTK_INPUT_MODE,
                                                        HILDON_GTK_INPUT_MODE_FULL |
+                                                       HILDON_GTK_INPUT_MODE_MULTILINE |
                                                        HILDON_GTK_INPUT_MODE_AUTOCAP |
                                                        HILDON_GTK_INPUT_MODE_DICTIONARY,
                                                        GTK_PARAM_READWRITE));
