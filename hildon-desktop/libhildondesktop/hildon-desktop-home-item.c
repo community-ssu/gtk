e9d637c57964466ea8946f61122aece9455c0fc3
Johan Bilien <johan.bilien@nokia.com>
2007-06-29 12:25:25 +0000

    2007-06-29  Johan Bilien  <johan.bilien@nokia.com>
    	* libhildondesktop/hildon-desktop-home-item.c:
    	- apply the background from the style in realize
    
    
    
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@12551 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- hildon-desktop/libhildondesktop/hildon-desktop-home-item.c
+++ hildon-desktop/libhildondesktop/hildon-desktop-home-item.c
@@ -717,6 +717,8 @@ hildon_desktop_home_item_realize (GtkWidget *widget)
     }
 
   widget->style = gtk_style_attach (widget->style, widget->window);
+  gtk_style_set_background (widget->style, widget->window, GTK_STATE_NORMAL);
+
 
 }
 
