26b9bcddbabed91e2f9fc68fffd1eea165b339b6
Marius Vollmer <marius.vollmer@nokia.com>
2007-07-18 16:36:48 +0000

    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@12770 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- hildon-fm/hildon-fm/t.c
+++ hildon-fm/hildon-fm/t.c
@@ -1,90 +1,22 @@
-#define GTK_FILE_SYSTEM_ENABLE_UNSUPPORTED
-
 #include <gtk/gtk.h>
-#include <gtk/gtkfilesystem.h>
-#include <libgnomevfs/gnome-vfs.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-
-GtkFileSystem *fs;
-
-static void
-list_gnome_vfs_volumes ()
-{
-  GnomeVFSVolumeMonitor *monitor;
-  GList *volumes, *drives;
-
-  monitor = gnome_vfs_get_volume_monitor ();
-
-  fprintf (stderr, "Root entries:\n");
-
-  volumes = gnome_vfs_volume_monitor_get_mounted_volumes (monitor);
-  while (volumes)
-    {
-      GnomeVFSVolume *volume = volumes->data;
-
-      if (gnome_vfs_volume_is_user_visible (volume)
-          && gnome_vfs_volume_is_mounted (volume))
-        fprintf (stderr,
-                 " %s %s\n",
-                 gnome_vfs_volume_get_device_path (volume),
-                 gnome_vfs_volume_get_activation_uri (volume));
-      else
-        fprintf (stderr,
-                 " (%s %s)\n",
-                 gnome_vfs_volume_get_device_path (volume),
-                 gnome_vfs_volume_get_activation_uri (volume));
-
-      volumes = volumes->next;
-    }
-
-  drives = gnome_vfs_volume_monitor_get_connected_drives (monitor);
-  while (drives)
-    {
-      GnomeVFSDrive *drive = drives->data;
-
-      if (gnome_vfs_drive_is_user_visible (drive)
-          && gnome_vfs_drive_is_connected (drive)
-          && !gnome_vfs_drive_is_mounted (drive))
-        fprintf (stderr,
-                 " %s\n",
-                 gnome_vfs_drive_get_device_path (drive));
-      else
-        fprintf (stderr,
-                 " (%s)\n",
-                 gnome_vfs_drive_get_device_path (drive));
-
-
-      drives = drives->next;
-    }
-
-  fprintf (stderr, "\n");
-
-}
-
-static void
-volumes_changed ()
-{
-  printf ("changed\n");
-  list_gnome_vfs_volumes ();
-}
+#include <libgnomevfs/gnome-vfs-init.h>
+#include <hildon-widgets/hildon-file-system-storage-dialog.h>
 
 int
-main (int argc, char **argv)
+main(int argc, char **argv)
 {
-  gtk_init (&argc, &argv);
+  GtkWidget *dlg = NULL;
 
-  fs = gtk_file_system_create ("gnome-vfs");
-  if (fs == NULL)
-    exit (1);
+  if (argc < 2) return 1;
 
-  volumes_changed ();
+  gtk_init(&argc, &argv);
 
-  g_signal_connect (fs, "volumes_changed",
-                    G_CALLBACK (volumes_changed), NULL);
+  gnome_vfs_init ();
 
-  gtk_main ();
+  gtk_dialog_run(GTK_DIALOG(dlg = hildon_file_system_storage_dialog_new(NULL,
+argv[1])));
+
+  gtk_widget_destroy(dlg);
+
   return 0;
 }
-
