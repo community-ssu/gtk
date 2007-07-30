2c405d86f09f04dee335072cedc8b7edaa9bb464
Marius Vollmer <marius.vollmer@nokia.com>
2007-07-30 15:11:53 +0000

    Func with show/hide
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk@12924 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- hildon-fm/hildon-fm/t.c
+++ hildon-fm/hildon-fm/t.c
@@ -1,22 +1,71 @@
 #include <gtk/gtk.h>
-#include <libgnomevfs/gnome-vfs-init.h>
-#include "hildon-file-system-storage-dialog.h"
+
+GtkWidget *scrolled (GtkWidget *);
+void clicked (GtkWidget *w, gpointer d);
+
+int cur;
+GtkWidget *labels[3];
+GtkWidget *hbox;
+
+GtkWidget *
+scrolled (GtkWidget *w)
+{
+  GtkWidget *scroller = gtk_scrolled_window_new (NULL, NULL);
+  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroller),
+				  GTK_POLICY_AUTOMATIC,
+				  GTK_POLICY_AUTOMATIC);
+  gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW (scroller),
+					 w);
+  return scroller;
+}
+
+void
+clicked (GtkWidget *w, gpointer d)
+{
+  gtk_widget_hide (labels[cur]);
+  cur = (cur + 1) % 3;
+  gtk_widget_show (labels[cur]);
+}
 
 int
 main(int argc, char **argv)
 {
-  GtkWidget *dlg = NULL;
+  int i;
 
-  if (argc < 2) return 1;
+  GtkWidget *window, *hpaned, *button;
+  
+  gtk_init (NULL, NULL);
 
-  gtk_init(&argc, &argv);
+  labels[0] = scrolled (gtk_label_new ("Label 1 - Arrrrrrrrrrrrrrrrrrrr"));
+  labels[1] = gtk_label_new ("Label 2 - Arrrrrrrrrrrrrrr");
+  labels[2] = scrolled (gtk_label_new ("Label 3 - Arrrrrrrrrrrrrrrrrrrrrrrrrrrrr"));
 
-  gnome_vfs_init ();
+  hbox = gtk_hbox_new (FALSE, 0);
+  for (i = 0; i < 3; i++)
+    gtk_box_pack_start (GTK_BOX (hbox), labels[i], TRUE, TRUE, 0);
 
-  gtk_dialog_run(GTK_DIALOG(dlg = hildon_file_system_storage_dialog_new(NULL,
-argv[1])));
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 
-  gtk_widget_destroy(dlg);
+  button = gtk_button_new_with_label ("Foooooooooooooooooooooooooooooooooooo");
+  g_signal_connect (button, "clicked",
+		    G_CALLBACK (clicked), NULL);
+
+  hpaned = gtk_hpaned_new ();
+  gtk_paned_pack1 (GTK_PANED (hpaned),
+		   scrolled (button),
+		   TRUE, FALSE);
+  gtk_paned_pack2 (GTK_PANED (hpaned),
+		   hbox,
+		   TRUE, FALSE);
+
+  gtk_container_add (GTK_CONTAINER (window), hpaned);
+
+  gtk_widget_show_all (window);
+  cur = 0;
+  gtk_widget_hide (labels[1]);
+  gtk_widget_hide (labels[2]);
+
+  gtk_main ();
 
   return 0;
 }
