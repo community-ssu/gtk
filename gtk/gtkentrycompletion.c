806c7241f6b3db40c5338c47ad28c5b3c79d17c3
Tommi Komulainen <tommi.komulainen@nokia.com>
2007-09-21 14:54:22 +0000

    Remove gtk grab from entrycompletion popup
    2007-09-21  Tommi Komulainen  <tommi.komulainen@nokia.com>
    
    	* gtk/gtkentrycompletion.c (_gtk_entry_completion_popup,
    	_gtk_entry_completion_popdown): Revert NB#66055 and remove the gtk
    	grab again. We want the user to be able use hardkeys and press toolbar
    	buttons while the popup is open. The application needs to ensure the
    	popup gets closed. NB#69655
    
    
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk/gtk+@14007 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- gtk/gtkentrycompletion.c
+++ gtk/gtkentrycompletion.c
@@ -1589,8 +1589,8 @@ _gtk_entry_completion_popup (GtkEntryCompletion *completion)
 
   gtk_widget_show (completion->priv->popup_window);
 
-  gtk_grab_add (completion->priv->popup_window);
 #ifndef MAEMO_CHANGES    
+  gtk_grab_add (completion->priv->popup_window);
   gdk_pointer_grab (completion->priv->popup_window->window, TRUE,
                     GDK_BUTTON_PRESS_MASK |
                     GDK_BUTTON_RELEASE_MASK |
@@ -1609,8 +1609,8 @@ _gtk_entry_completion_popdown (GtkEntryCompletion *completion)
 
 #ifndef MAEMO_CHANGES
   gdk_pointer_ungrab (GDK_CURRENT_TIME);
-#endif
   gtk_grab_remove (completion->priv->popup_window);
+#endif
 
   gtk_widget_hide (completion->priv->popup_window);
 }
