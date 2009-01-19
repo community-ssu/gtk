77eb129c0d2c69e8bbb0ff72cf39ecacd565da85
Kristian Rietveld <kris@lanedo.com>
2009-01-19 15:09:27 +0000

    2009-01-19  Kristian Rietveld  <kris@imendio.com>
    	Fixes: NB#93161 - GTKTextView scrolls to cursor on size change
    	even if text view non-editable.
    
    	* gtk/gtktextview.c (gtk_text_view_size_allocate): only scroll
    	to cursor if the cursor is visible.
    
    
    
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk/gtk+@17234 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- gtk/gtktextview.c
+++ gtk/gtktextview.c
@@ -3420,11 +3420,14 @@ gtk_text_view_size_allocate (GtkWidget *widget,
       gdk_window_invalidate_rect (widget->window, NULL, FALSE);
 
 #ifdef MAEMO_CHANGES
-      /* keep cursor visible (when IM opens, for instance) */
-      gtk_text_view_scroll_to_mark (text_view,
-                                    gtk_text_buffer_get_mark (get_buffer (text_view),
-                                                              "insert"),
-                                    0.0, FALSE, 0.0, 0.0);
+      /* Keep cursor visible (when IM opens, for instance), but
+       * only if the cursor is not hidden.
+       */
+      if (text_view->cursor_visible)
+        gtk_text_view_scroll_to_mark (text_view,
+                                      gtk_text_buffer_get_mark (get_buffer (text_view),
+                                                                "insert"),
+                                      0.0, FALSE, 0.0, 0.0);
 #endif /* MAEMO_CHANGES */
     }
 }
