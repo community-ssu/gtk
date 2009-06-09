24b1f2ce765ad85ee55b0d19bde1f929642f4ab6
Alejandro Pinheiro <apinheiro@igalia.com>
2009-06-09 14:56:43 +0000

    2009-06-09 Alejandro Pinheiro <apinheiro@igalia.com>
    	* gail/gailwidget.c:
    	(gail_widget_all_parents_visible): auxiliar function that checks
    	if all parent hierarchy is visible
    	(gail_widget_ref_state_set): checks parent visibility in order
    	to add the state ATK_STATE_SHOWING to the current state set
    
    	Applying the patch related to bug NB#76325 (SPI_STATE_SHOWING flag
    	is used for element of Bookmark menu incorrectly) on the gtk trunk
    
    	Maemo solution while upstream bug #509650 is not solved
    
    
    
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk/gtk+@18683 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- modules/other/gail/gailwidget.c
+++ modules/other/gail/gailwidget.c
@@ -103,6 +103,9 @@ static void       gail_widget_real_initialize    (AtkObject     *obj,
                                                   gpointer      data);
 static GtkWidget* gail_widget_find_viewport      (GtkWidget     *widget);
 static gboolean   gail_widget_on_screen          (GtkWidget     *widget);
+#ifdef MAEMO_CHANGES
+static gboolean   gail_widget_all_parents_visible(GtkWidget     *widget);
+#endif
 
 G_DEFINE_TYPE_WITH_CODE (GailWidget, gail_widget, GTK_TYPE_ACCESSIBLE,
                          G_IMPLEMENT_INTERFACE (ATK_TYPE_COMPONENT, atk_component_interface_init))
@@ -515,7 +518,11 @@ gail_widget_ref_state_set (AtkObject *accessible)
       if (GTK_WIDGET_VISIBLE (widget))
         {
           atk_state_set_add_state (state_set, ATK_STATE_VISIBLE);
+#ifdef MAEMO_CHANGES
+          if (gail_widget_on_screen (widget) && gail_widget_all_parents_visible (widget) &&
+#else
           if (gail_widget_on_screen (widget) &&
+#endif
               GTK_WIDGET_MAPPED (widget))
             {
               atk_state_set_add_state (state_set, ATK_STATE_SHOWING);
@@ -1076,3 +1083,33 @@ static gboolean gail_widget_on_screen (GtkWidget *widget)
 
   return return_value;
 }
+
+
+#ifdef MAEMO_CHANGES
+/**
+ * gail_widget_all_parents_visible:
+ * @widget:
+ *
+ * Checks if the parent widget, and his parent, etc is all visible
+ * Used to check the
+ *
+ * Return value: TRUE if all the parent hierarchy is visible, FALSE otherwise
+ **/
+static gboolean   gail_widget_all_parents_visible(GtkWidget     *widget)
+{
+  GtkWidget *iter_parent = NULL;
+  gboolean result = TRUE;
+
+  for ( iter_parent = gtk_widget_get_parent(widget); iter_parent;
+        iter_parent = gtk_widget_get_parent(iter_parent))
+    {
+      if (!GTK_WIDGET_VISIBLE(iter_parent))
+        {
+          result = FALSE;
+          break;
+        }
+    }
+
+  return result;
+}
+#endif
